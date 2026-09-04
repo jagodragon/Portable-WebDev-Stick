824b019f58a98658b9ce24658e6cb2e0e7b308b1
jagodragon <rphillip@live.com>
2026-09-04 00:11:50 -0400

    bug fixes

--- OS_Windows/Scripts/StopCaddy.bat
+++ OS_Windows/Scripts/StopCaddy.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 🛑 Stopping Windows Production Caddy and FastCGI Processes...
 :: Forcefully terminates both the Windows Caddy engine and the FastCGI background manager
-taskkill /f /im caddy.exe
-taskkill /f /im php-cgi.exe
+taskkill /f /im caddy_windows_amd64.exe >nul 2>&1
+taskkill /f /im php-cgi.exe >nul 2>&1
